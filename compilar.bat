9ce5d6815abca21b2bfc08dc2cd7340a3af9a9d4
Pablo Caiza <pablocuri2015@hotmail.com>
2023-02-20 16:05:39 -0500

    acces to dusame

--- compilar.bat
+++ compilar.bat
@@ -1,2 +1,2 @@
-git clone https://github.com/PabloCaiza/library_microservices_v2.git && cd ./library_microservices_v2 && cd ./app-books && gradlew libertyPackage && docker build -t pjcaiza/app-books:1.0.0 . && cd .. && cd ./app-authors && docker build -t pjcaiza/app-authors:1.0.0 . && cd .. && docker push pjcaiza/app-books:1.0.0 && docker push pjcaiza/app-authors:1.0.0 && docker compose up
+git clone https://github.com/PabloCaiza/library_microservices_v2.git && cd ./library_microservices_v2 && cd ./app-books && gradlew libertyPackage && docker build -t ddusama/app-books:1.0.0 . && cd .. && cd ./app-authors && docker build -t ddusama/app-authors:1.0.0 . && cd .. && docker push ddusama/app-books:1.0.0 && docker push ddusama/app-authors:1.0.0 && docker compose up
 
